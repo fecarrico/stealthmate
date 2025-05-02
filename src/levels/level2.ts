b327c272449ad19ca9df11a1c78daf55bf2d02bc
fecarrico <153565622+fecarrico@users.noreply.github.com>
2025-05-02 15:16:16 -0300

    Update level2.ts

--- src/levels/level2.ts
+++ src/levels/level2.ts
@@ -4,15 +4,30 @@ import { LevelData, CellType } from '../utils/levelData';
 const level2: LevelData = {
   id: 2,
   level: 2,
-  name: "Level 2",
-  playerStart: [0, 0] as [number, number],
-  kings: [[4, 4]] as [number, number][],
+  name: "Werehouse",
+  playerStart: [0, 0],
+  kings: [[5, 5]],
   enemies: [
-    { type: CellType.BISHOP, position: [4, 0] as [number, number] },
-    { type: CellType.BISHOP, position: [0, 4] as [number, number] },
+    { type: CellType.ROOK, position: [0, 5] },
+    { type: CellType.BISHOP, position: [5, 6] }
   ],
-  boxes: [[1, 1], [1, 2], [2, 1]] as [number, number][],
-  board: [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]],
+  boxes: [
+    [0,1], [0,2], [0,3], [0,4], [0,6],
+    [1,1], [1,2], [1,3], [1,4],
+    [2,1], [2,2], [2,3], [2,4],
+    [3,1],
+    [4,1], [4,2],
+    [6,0], [6,1], [6,2], [6,3]
+  ],
+  board: [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+  ]
 };
 
 export default level2;
