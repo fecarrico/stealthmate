72865d217e5ec5ad49c46e22e38bd9ac9a85311e
fecarrico <153565622+fecarrico@users.noreply.github.com>
2025-05-02 15:31:09 -0300

    Update level3.ts

--- src/levels/level3.ts
+++ src/levels/level3.ts
@@ -3,21 +3,31 @@ import { LevelData, CellType } from '../utils/levelData';
 const level3: LevelData = {
   id: 3,
   level: 3,
-  name: 'Level 3',
-  board: [
-    [0, 0, 0, 0, 0],
-    [0, 1, 0, 2, 0],
-    [0, 0, 3, 0, 0],
-    [0, 4, 0, 0, 0],
-    [0, 0, 0, 0, 0],
+  name: "God save me!",
+  playerStart: [0, 0],
+  kings: [
+    [1, 6],
+    [6, 1]
   ],
-  playerStart: [1, 1],
   enemies: [
-    { type: CellType.QUEEN, position: [4, 0] }
+    { type: CellType.BISHOP, position: [0, 6] },
+    { type: CellType.BISHOP, position: [6, 0] },
+    { type: CellType.QUEEN, position: [6, 6] }
   ],
   boxes: [
-    [3, 3],
-    [2, 3],
+    [0,1], [0,2], [0,3],
+    [1,1],
+    [2,0], [2,1], [2,2], [2,3],
+    [3,0], [3,1], [3,2]
   ],
+  board: [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+  ]
 };
 export default level3;
